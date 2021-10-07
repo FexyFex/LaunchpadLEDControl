d6b43dd68c30411fc535426149b62a391da318fc
FexyFex <fynnman@freenet.de>
2021-10-07 19:17:04 +0200

    more colors

--- src/main/kotlin/launchpad/Color.kt
+++ src/main/kotlin/launchpad/Color.kt
@@ -5,9 +5,16 @@ enum class Color(val num: Byte) {
     DARK_GREY(1),
     LIGHT_GREY(2),
     WHITE(3),
-    WEIRD_RED(4),
+    BABY_RED(4),
     RED(5),
+    DARK_RED(6),
+    GREY_BROWN(7),
+    BEIGE(8),
+    ORANGE(9),
+    BROWN(10),
+    GREEN_BROWN(11),
+    LIGHT_GREEN(12),
     YELLOW(13),
     LIME(113),
-    DARK_RED(127)
+    BROWN_RED(127)
 }
